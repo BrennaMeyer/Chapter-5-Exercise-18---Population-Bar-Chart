80a52908eec283c6a273bc006d09d76a6fb4c37d
Brenna Meyer <brennameyer9@gmail.com>
2025-03-20 11:31:51 -0400

    done

--- Chapter 5 Exercise 18 - Population Bar Chart.cpp
+++ Chapter 5 Exercise 18 - Population Bar Chart.cpp
@@ -7,19 +7,35 @@
 // it should display the date and a bar consisting of one asterisk for each 1,000 people. The data can be found in the People.txt file.
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ifstream inputFile;
+	string years;
+
+	cout << "PRAIRIEVILLE POPULATION GROWTH\n";
+	cout << "(each * represents 1,000 people)\n";
+
+	inputFile.open("People.txt");
+	if (inputFile)
+	{
+		while (getline(inputFile, years))
+		{
+			cout << years << endl;
+		}
+	}
+	else
+	{
+		cout << "Error opening file." << endl;
+	}
+
+	inputFile.close();
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
