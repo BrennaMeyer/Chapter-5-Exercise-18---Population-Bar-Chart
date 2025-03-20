5b3b93d2a0357aafb42e6305aded2a13aeb410a3
Brenna Meyer <brennameyer9@gmail.com>
2025-03-20 11:08:23 -0400

    Add project files.

---
+++ Chapter 5 Exercise 18 - Population Bar Chart.cpp
@@ -0,0 +1,25 @@
+// Chapter 5 Exercise 18 - Population Bar Chart.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer :Brenna Meyer 
+//Date: March 20th,2025
+//Requirements:Write a program that produces a bar chart showing the population growth of Prairieville, a small town in the Midwest,
+// at 20-year intervals during the past 100 years. The program should read in the population figures 
+// (rounded to the nearest 1,000 people) for 1900, 1920, 1940, 1960, 1980, and 2000 from a file. For each year, 
+// it should display the date and a bar consisting of one asterisk for each 1,000 people. The data can be found in the People.txt file.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
